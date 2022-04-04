8b98e4024cec8beb619c036ec912e0a94aa95f73
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-04-04 21:35:56 +0700

    Create orderViewModel

---
+++ Coffee Ordering/View Models/OrderViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  OrderViewModel.swift
+//  Coffee Ordering
+//
+//  Created by TheGIZzz on 4/4/2565 BE.
+//
+
+import Foundation
+
+class OrderListViewModel {
+
+    var ordersViewModel: [OrderViewModel]
+    
+    init() {
+        self.ordersViewModel = [OrderViewModel]() //empty array
+    }
+}
+
+extension OrderListViewModel {
+    func orderViewModel(dd index: Int) -> OrderViewModel {
+        return self.ordersViewModel[index]
+    }
+}
+
+struct OrderViewModel {
+    let order: Order
+}
+
+extension OrderViewModel {
+    
+    var name : String {
+        return self.order.name
+    }
+    
+    var email : String {
+        return self.order.email
+    }
+    
+    var type : String {
+        return self.order.type.rawValue.capitalized
+    }
+    
+    var size : String {
+        return self.order.size.rawValue.capitalized
+    }
+}
